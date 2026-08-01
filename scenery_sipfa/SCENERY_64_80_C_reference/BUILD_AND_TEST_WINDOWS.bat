8e26d4a693ae419c173fb8176db2c24367d8946d
vahid soleimani <vahidsoleimani.tc@gmail.com>
2026-08-01 16:20:52 +0330

    Delete scenery_sipfa/SCENERY_64_80_C_reference directory

--- scenery_sipfa/SCENERY_64_80_C_reference/BUILD_AND_TEST_WINDOWS.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-setlocal
-if not exist build-windows mkdir build-windows
-cmake -S . -B build-windows
-if errorlevel 1 exit /b 1
-cmake --build build-windows --config Release
-if errorlevel 1 exit /b 1
-ctest --test-dir build-windows -C Release --output-on-failure
